b42eee4f24467c755ea31aa3f0b6a9950d1e2e87
vibhorag101 <vibhor20349@iiitd.ac.in>
2021-11-17 21:08:26 +0530

    Found Working Process for Alarm

--- Assignment_2/Problem_1/SRHandler.c
+++ Assignment_2/Problem_1/SRHandler.c
@@ -5,6 +5,7 @@
 #include <signal.h>
 #include <errno.h>
 #include <sys/time.h>
+#define INTERVAL 500
 pid_t pidS1 =-1;
 void alarmHandler();
 int main(int argc, char const *argv[])
@@ -12,17 +13,20 @@ int main(int argc, char const *argv[])
     struct itimerval SRTimer;
     pidS1= atoi(argv[1]);
     signal(SIGALRM,alarmHandler);
-    SRTimer.it_interval.tv_sec=1;
-    SRTimer.it_interval.tv_usec=100000;
-    SRTimer.it_value = SRTimer.it_interval;
+    SRTimer.it_value.tv_sec= INTERVAL/1000;
+    SRTimer.it_value.tv_usec=(INTERVAL*1000) % 1000000;
+    SRTimer.it_interval = SRTimer.it_value;
     int a = setitimer(ITIMER_REAL,&SRTimer,NULL);
-    printf("The value is %d\n",a);
-    printf("We are in SRHandler file pid of S1 is %s\n",argv[1]);
-    printf("insider\n");
+    printf("We are in SRHandler file\n");
+    while (1)
+    {
+        pause();
+    }
+    
     return 0;
 }
 void alarmHandler(){
-    printf("insider\n");
+    printf("Alarm from SR\n");
     kill(pidS1,SIGTERM);
 
 }
--- Assignment_2/Problem_1/STHandler.c
+++ Assignment_2/Problem_1/STHandler.c
@@ -3,9 +3,30 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <signal.h>
+#include <errno.h>
+#include <sys/time.h>
+#define INTERVAL 500
+pid_t pidS1 =-1;
+void alarmHandler();
 int main(int argc, char const *argv[])
 {
-    printf("We are in STHandler file\n");
+    struct itimerval SRTimer;
+    pidS1= atoi(argv[1]);
+    signal(SIGALRM,alarmHandler);
+    SRTimer.it_value.tv_sec= INTERVAL/1000;
+    SRTimer.it_value.tv_usec=(INTERVAL*1000) % 1000000;
+    SRTimer.it_interval = SRTimer.it_value;
+    int a = setitimer(ITIMER_REAL,&SRTimer,NULL);
+    printf("We are in SRHandler file\n");
+    while (1)
+    {
+        pause();
+    }
     
     return 0;
+}
+void alarmHandler(){
+    printf("Alarm form ST\n");
+    kill(pidS1,SIGTERM);
+
 }
