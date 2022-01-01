3e26e7d20134a1b80b3a8ced1487c426abd0517a
Vibhor <vibhor20349@iiitd.ac.in>
2022-01-01 17:02:28 +0530

    Some testing

--- Assignment_5/forkSauce.c
+++ Assignment_5/forkSauce.c
@@ -3,7 +3,6 @@
 #include <semaphore.h>
 #include <pthread.h>
 #include <unistd.h>
-pthread_t philosopher[5];
 sem_t forks[5];
 sem_t sauceBowls[4];
 pthread_mutex_t waiter;
@@ -11,14 +10,18 @@ void getForks(int philosopherArrived)
 {
     sem_wait(&forks[philosopherArrived]);
     sem_wait(&forks[(philosopherArrived + 1) % 5]);
+    printf("got forks %d\n", philosopherArrived);
 }
 int getBowl()
 {
     for (int i = 0; i < 4; i++)
     {
         int semValue = -1;
-        if (sem_getvalue(&sauceBowls[i], &semValue) == 1)
+        int err = sem_getvalue(&sauceBowls[i], &semValue);
+        if (semValue == 1)
         {
+            printf("got bowl\n");
+            // printf("returned + %d\n",i);
             sem_wait(&sauceBowls[i]);
             return (i);
         }
@@ -32,18 +35,26 @@ void eat(int philosopherArrived, int bowlAcquired)
     sem_post(&sauceBowls[philosopherArrived]);
     sem_post(&sauceBowls[(philosopherArrived + 1) % 5]);
     sem_post(&sauceBowls[bowlAcquired]);
+    sleep(1);
 }
 void *philosopherAction(void *philosopherIndex)
 {
     int philosopherArrived = *((int *)philosopherIndex);
-    pthread_mutex_lock(&waiter);
-    getForks(philosopherArrived);
-    int acquiredBowl = getBowl();
-    pthread_mutex_unlock(&waiter);
-    eat(philosopherArrived, acquiredBowl);
+    while(1)
+    {
+        printf("%d arrived\n", philosopherArrived);
+        pthread_mutex_lock(&waiter);
+        getForks(philosopherArrived);
+        int acquiredBowl = getBowl();
+        pthread_mutex_unlock(&waiter);
+        printf("%d got success in getting\n", philosopherArrived);
+        eat(philosopherArrived, acquiredBowl);
+        printf("%d done and exit\n");
+    }
 }
 int main()
 {
+    pthread_t philosopher[5];
     pthread_mutex_init(&waiter, NULL);
     //creating forks
     for (int i = 0; i < 5; i++)
@@ -57,7 +68,7 @@ int main()
     }
     for (int i = 0; i < 5; i++)
     {
-        pthread_create(&philosopher[i], NULL, &philosopherAction, (void *)&i);
+        pthread_create(&philosopher[i], NULL, philosopherAction, (void *)&i);
     }
     for (int i = 0; i < 5; i++)
     {
